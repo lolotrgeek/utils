5b7a3dea5f196b84c1f7ee81357b04a50b8fa106
lolotrgeek <jon@simplyenable.com>
2022-04-23 13:12:40 -0500

    feat: update test_runner

--- test_runner.js
+++ test_runner.js
@@ -6,7 +6,6 @@ const directoryPath = path.join(__dirname, '/tests/')
 
 // failure on timeout
 
-//** This loads all files from a directory and runs them, listens for pass/fail messages */
 fs.readdir(directoryPath, (err, files) => {
     if (err) console.log(err)
     let count = 1
@@ -31,16 +30,21 @@ const timeout = 30000
  * @note best to call this from the test function and run it separately from the above function.
  * @param {function} test 
  */
-function run(test) {
+function run_test(test) {
     if (process.send) process.send(`Starting : ${test.name}`)
     let timer = setTimeout(() => {
         process.send ? process.send({ name: test.name, result: false, reason:'Timeout' }) : console.log({name: test.name, result: false, reason:'Timeout'})
+        process.send(`Ending : ${test.name}`)
         process.exit()
     }, timeout)
 
-    test(result => {
-        let report = { name: test.name, result, reason: 'Internal' }
+    /**
+     * Usage: in the test, give a callback(result, reason), see sample_test
+     */
+    test((result, reason = 'Internal') => {
+        let report = { name: test.name, result, reason }
         process.send ? process.send(report) : console.log(report)
+        process.send(`Ending : ${test.name}`)
         process.exit()
     })
     process.on('message', message => {
@@ -50,39 +54,8 @@ function run(test) {
         }
     })
 }
-module.exports = { run }
 
-//////
-// function Test(file) {
-//     return new Promise((resolve, reject) => {
-//         if (file === 'utils') resolve()
-//         let child = fork(directoryPath + file, { stdio: ['ignore', 'ignore', 'ipc'] })
-//         child.on('message', message => {
-//             console.log(message)
-//             resolve(message)
-//         })
-//     })
-// }
-
-// async function Iterate(files) {
-//     await files.reduce(async (previous, current, index) => {
-//         try {
-//             await previous
-//             await Test(current)
-//         } catch (error) {
-//             console.log(error)
-//         }
-//     }, Promise.resolve())    
-// }
-
-// async function Begin() {
-//     return new Promise((resolve, reject) => fs.readdir(directoryPath, (err, files) => {
-//         if (err) reject (err)
-//         resolve( files)
-//     }))
-// }
-
-// async function Run() {
-//     let tests = await Begin()
-//     await Iterate(tests)
-// }
+function sample_test(callback) {
+    callback(true, "because")
+}
+module.exports = { run_test }
