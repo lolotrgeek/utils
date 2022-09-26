f9a33f138fb4607644f710eebc4cdf7f5ffdbcf4
lolotrgeek <jon@simplyenable.com>
2022-09-26 09:54:40 -0500

    refactor: equals

--- equals.js
+++ equals.js
@@ -1,9 +1,7 @@
 /**
  * Determine if two arrays are equal (non-sequential)
- * @param {*} a 
- * @param {*} b 
+ * @param {array} a 
+ * @param {array} b 
  * @returns 
  */
- const equals = (a, b) =>
- a.length === b.length &&
- a.every((v, i) => v === b[i])
+const equals = (a, b) => a.length === b.length && a.every((v, i) => v === b[i])
