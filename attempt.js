9481403d92284b4e33f284e28261cac8a3c4efee
lolotrgeek <jon@simplyenable.com>
2022-10-03 14:44:01 -0500

    fix: handleError for attempt

--- attempt.js
+++ attempt.js
@@ -1,13 +1,24 @@
 /**
+ * returns a reasoned error object
+ * @param {*} reason 
+ * @param {*} error 
+ * @returns 
+ */
+ function handleError(reason, error) {
+    log({reason, error })
+    return { reason, error }
+}
+
+/**
  * Attempts to run a function `func`, any arguments after `func` are passed to `func`
  * @param {function} func 
  * @returns 
  */
-async function attempt(func) {
-    if (typeof func !== 'function') return handleError("Cannot attempt.", func)
+async function attempt (func) {
+    if(typeof func !== 'function') return handleError("Cannot attempt.", func)
     let args = [...arguments].slice(1)
     let result = await func(...args)
-    if (result.code) return handleError(`Could not run ${func.name}`, result)
+    if(result.code || result.error) return handleError(`Could not run ${func.name}`, result)
     return result
 }
 
